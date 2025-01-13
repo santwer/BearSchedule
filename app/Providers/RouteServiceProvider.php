20ed917816f08d91bdaa67e4f3cf55ae7f508c65
Sven <kennx@hotmail.de>
2025-01-13 21:15:20 +0200

    remove api

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -42,7 +42,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        $this->mapApiRoutes();
+        //$this->mapApiRoutes();
 
         $this->mapWebRoutes();
 
